ba2c3a3de08378c3afc9536d9ee4658ea173dcb3
Priscilla Arinze <priscillaarinze@gmail.com>
2023-03-08 00:29:33 -0600

    Finished adding stuff to basic syntax review

--- BasicSyntax/Program.cs
+++ BasicSyntax/Program.cs
@@ -70,6 +70,68 @@ class Program
 
 
 
+        //// CONDITIONAL STATEMENTS
+        
+        // if - else if - else Statement
+        if (a % 2 == 0) {
+            Console.WriteLine($"{a} is even.");
+        }
+        else if (a % 2 != 0) {
+            Console.WriteLine($"{a} is odd.");
+        }
+        else {
+            Console.WriteLine("not a number i guess");
+        }
+
+        //Switch Statement
+        string hungry = "yes";
+
+        switch(hungry) {
+            case "yes":
+                Console.WriteLine("food food food");
+                break;
+            case "no":
+                Console.WriteLine("no food no food no food");
+                break;
+            default:
+                Console.WriteLine("food?");
+                break;
+        }
+
+
+
+        //// LOOPS
+
+        //while Loop
+        int studentloandebt = 9000000;
+
+        while (studentloandebt > 0) {
+            Console.WriteLine($"Student Loan Debt Left: {studentloandebt}");
+
+            studentloandebt -= 1000000;
+        }
+
+        //do-while Loop
+        do {
+            Console.WriteLine("you may still owe millions in student loans");
+            studentloandebt -= 1000000;
+        }
+        while (studentloandebt > 0);
+
+        //for Loop
+        for(int i = 5; i >= 0; i--){
+            Console.WriteLine($"T-Minus {i}");
+        }
+
+        //foreach Loop (iterating through arrays/collections) == Python's 'in' keyword in for loops
+        var numbers_words = new List<string>(new[] { "five", "four", "three", "two", "one", "zero" });
+        foreach(string word in numbers_words) {
+            Console.WriteLine(word);
+        }
+
+
+
+
         //// ARRAYS/LISTS
         var numbers = new List<int>(new[] { 1, 2, 3 });
         
@@ -79,16 +141,21 @@ class Program
 
 
 
+
         //// STRINGS
+        /* NOTE: Strings are immutable */
         string str2 = "i am a string";
 
         //String Interpolation (format strings)
         Console.WriteLine($"here is a random string i entered previously: {str}");
         Console.WriteLine("\n");
 
-        //Indexing
+        //Indexing (by number) --> char
         char ch = str[str.Length - 1];
-        Console.WriteLine($"Last character in previously entered string: {ch}");
+        Console.WriteLine($"*Indexing by Number* --- Last character in previously entered string: {ch}");
+
+        //Indexing (by character/string) --> index
+        Console.WriteLine($"*Indexing by character/string* --- Index of the last charcter of the previously entered string: {str.IndexOf(ch)}");
 
         //Concatenation
         string str3 = "i " + "am " + "doing " + "the " + "contenating ";
@@ -105,9 +172,26 @@ class Program
         Console.WriteLine(date);
 
         //Length
-        Console.WriteLine($"The previously entered string this {str.Length} letters in it.");
+        Console.WriteLine($"*String Length* --- The previously entered string this {str.Length} letters in it.");
 
-        //Trim
+        //Substring
+        Console.WriteLine($"A substring of 'Bread': {words[2].Substring(1, words[2].Length-1)}");
 
+        //Contains (boolean) & Replace
+        string str4 = "evading taxes <3";
+        
+        if (str4.Contains("evading")) {
+            Console.WriteLine(str4.Replace("evading", "paying"));
+        }
+
+        //Raw (verbatim) strings
+        Console.WriteLine(@"I \ am \ a \ literal \ raw \ string \ right \ now \n\t");
+
+        //Trim (start & end)
+        string str5 = "*************i am a string with extra characters******************";
+        string str6 = str5.Replace('*', ' '); // output: "                i am a string with extra characters              "
+        Console.WriteLine(str6);
+        Console.WriteLine($"Length of string with whitespaces: {str6.Length}");
+        Console.WriteLine($"New string: {str6.Trim(' ')}, New length: {str6.Trim(' ').Length}");
     }
 }
