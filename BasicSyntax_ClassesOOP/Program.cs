120df60b23bfe5d51a2334038df71abbe21733f1
Priscilla Arinze <priscillaarinze@gmail.com>
2023-04-10 01:08:44 -0500

    -Updated namespaces syntax to stay consistent with .NET 5.0 in BasicSyntax_ClassesOOP project -Started a C# resources notebook

--- BasicSyntax_ClassesOOP/Program.cs
+++ BasicSyntax_ClassesOOP/Program.cs
@@ -1,49 +1,54 @@
-﻿namespace BasicSyntax_ClassesOOP;
-class Program
-{
-    static void Main(string[] args)
+﻿using System;
+using System.Text;
+
+namespace BasicSyntax_ClassesOOP {
+    class Program
     {
-        var account1 = new BankAccount("Squidward", 260);
-        Console.WriteLine($"New bank account for {account1.Owner} [Account ID #{account1.ID}], starting with ${account1.Balance}");
+        static void Main(string[] args)
+        {
+            var account1 = new BankAccount("Squidward", 260);
+            Console.WriteLine($"New bank account for {account1.Owner} [Account ID #{account1.ID}], starting with ${account1.Balance}");
 
 
-        Console.WriteLine("");
-        
+            Console.WriteLine("");
+            
 
-        account1.MakeWithdrawal(75, DateTime.Now, "Clarinet");
-        Console.WriteLine($"$75 has been debited from {account1.Owner}'s account. Remaining balance: ${account1.Balance}");
-
-
-        Console.WriteLine("");
+            account1.MakeWithdrawal(75, DateTime.Now, "Clarinet");
+            Console.WriteLine($"$75 has been debited from {account1.Owner}'s account. Remaining balance: ${account1.Balance}");
 
 
-        // Handling exceptions (try/catch)
-        try{
-            account1.MakeDeposit(-300, DateTime.Now, "tax fraud");
+            Console.WriteLine("");
+
+
+            // Handling exceptions (try/catch)
+            try{
+                account1.MakeDeposit(-300, DateTime.Now, "tax fraud");
+            }
+            catch (ArgumentOutOfRangeException e){
+                Console.WriteLine("Attempting to deposit a negative amount, cancelling transaction...");
+                Console.WriteLine($"Exception is as follows: {e.ToString()}");
+            }
+
+
+            Console.WriteLine("");
+
+
+            try{
+                account1.MakeWithdrawal(9175, DateTime.Now, "Rent");
+                Console.WriteLine($"$9175 has been debited from {account1.Owner}'s account. Remaining balance: ${account1.Balance}");
+            }
+            catch (InvalidOperationException e){
+                Console.WriteLine("You're broke INSUFFICIENT FUNDS");
+                Console.WriteLine($"Exception is as follows: {e.ToString()}");
+            }
+
+
+            Console.WriteLine("");
+
+
+            Console.WriteLine(account1.GetTransactionStatement());
+            
         }
-        catch (ArgumentOutOfRangeException e){
-            Console.WriteLine("Attempting to deposit a negative amount, cancelling transaction...");
-            Console.WriteLine($"Exception is as follows: {e.ToString()}");
-        }
-
-
-        Console.WriteLine("");
-
-
-        try{
-            account1.MakeWithdrawal(9175, DateTime.Now, "Rent");
-            Console.WriteLine($"$9175 has been debited from {account1.Owner}'s account. Remaining balance: ${account1.Balance}");
-        }
-        catch (InvalidOperationException e){
-            Console.WriteLine("You're broke INSUFFICIENT FUNDS");
-            Console.WriteLine($"Exception is as follows: {e.ToString()}");
-        }
-
-
-        Console.WriteLine("");
-
-
-        Console.WriteLine(account1.GetTransactionStatement());
-        
     }
 }
+
