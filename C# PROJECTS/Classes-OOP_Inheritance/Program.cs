23667708a43d38663c1015c8bc2e9b34c903e6d4
Priscilla Arinze <priscillaarinze@gmail.com>
2023-05-29 18:11:03 -0500

    More info about transitive inheritance

--- C# PROJECTS/Classes-OOP_Inheritance/Program.cs
+++ C# PROJECTS/Classes-OOP_Inheritance/Program.cs
@@ -6,7 +6,10 @@ class Program
         // CONSIDERATIONS:
             // Inheritance is only supported for classes and interfaces; not structs, delegates, enums
                 // NOTE: structs can implement interfaces
+
             // C# only supported single class inheritance, cannot inherit more than one at once for the same class
                 // NOTE: classes can implement multiple interfaces  
+
+            // Inhertiance is transitive: A --> B --> C   ==   A --> C 
     }
 }
