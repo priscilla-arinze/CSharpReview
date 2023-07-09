1e6fc78820d6c8dddfbf735fd78f222240549e5b
Priscilla Arinze <priscillaarinze@gmail.com>
2023-07-09 00:10:49 -0500

    Added XUnit project for BankAccount class functionality and exception handling unit tests

---
+++ C# PROJECTS/Classes-OOP_MAIN/BankAccountTests/BankUnitTests.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Text.RegularExpressions;
+using BankAccountCoreLib;
+
+namespace BankAccountTests;
+
+public class BankUnitTests
+{
+    [Fact]
+    public void WhenDeposit_ShouldIncreaseBalance()
+    {
+        BankAccount testaccount = new BankAccount("TestAccount", 100);
+
+        decimal initialBalance = testaccount.Balance;
+
+        testaccount.MakeDeposit(10, DateTime.Now, "very successful bank robbery");
+
+        Assert.Equal(initialBalance+10, 110);
+    }
+    
+    [Fact]
+    public void WhenWithdrawal_ShouldDecreaseBalance()
+    {
+        BankAccount testaccount = new BankAccount("TestAccount", 100);
+
+        decimal initialBalance = testaccount.Balance;
+
+        testaccount.MakeWithdrawal(95, DateTime.Now, "gambling");
+
+        Assert.Equal(initialBalance-95, 5);
+    }
+
+    [Fact]
+    public void ShouldSaveMultipleTransactions()
+    {
+        BankAccount testaccount = new BankAccount("TestAccount", 100); //initial transaction
+        testaccount.MakeDeposit(7, DateTime.Now, "massive salary direct deposit");
+        testaccount.MakeWithdrawal(20, DateTime.Now, "groceries");
+        testaccount.MakeWithdrawal(5, DateTime.Now, "some more groceries");
+        testaccount.MakeDeposit(500, DateTime.Now, "gambling winnings");
+        testaccount.MakeWithdrawal(550, DateTime.Now, "irs back taxes <3");
+
+        string transactionstmt = testaccount.GetTransactionStatement();
+
+        Regex pattern = new Regex(@$"{DateTime.Now.ToShortDateString()}");
+        MatchCollection matches = pattern.Matches(transactionstmt);
+        Assert.Equal(matches.Count, 6);
+
+    }
+
+    [Fact]
+    public void WhenNegativeDeposit_ThrowsException()
+    {
+        BankAccount testaccount = new BankAccount("TestAccount", 100);
+
+        // Anonymous function
+        Assert.Throws<ArgumentOutOfRangeException>(
+            () => testaccount.MakeDeposit(-300, DateTime.Now, "negative deposit glitch")
+        );
+    }
+
+    [Fact]
+    public void WhenInsufficientFunds_ThrowsException()
+    {
+        BankAccount testaccount = new BankAccount("TestAccount", 100);
+
+        // Anonymous function
+        Assert.Throws<InvalidOperationException>(
+            () => testaccount.MakeWithdrawal(1275, DateTime.Now, "bananas")
+        );
+    }
+}
