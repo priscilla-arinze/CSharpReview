4476f26a47cbab0793ce4e093e7c082ed927052c
Priscilla Arinze <priscillaarinze@gmail.com>
2023-09-13 14:19:17 -0500

    Added .NET 6 C# project version of PasswordChecker notebook with LINQ & Regex solutions

---
+++ EXERCISES/PasswordCheckerNET6/PasswordCheckerValidation.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PasswordCheckerNET6
+{
+    public class PasswordCheckerValidation
+    {
+        /// <summary> Returns a boolean of whether the given password passes the following validation criteria using LINQ
+        /// <para>Validation Criteria:</para>
+        /// <para>1. Must be between 8-30 characters in length</para>
+        /// <para>2. Must contain at least 1 number</para>
+        /// <para>3. Must contain at least 1 capital letter (A-Z)</para>
+        /// <para>4. Must contain at least 1 symbol (includes spaces)</para>
+        /// <para>5. Must NOT contain the word "password", regardless of case</para>
+        /// </summary>
+        public bool ValidatePasswordLINQ(string password)
+        {
+            int passwordLength = password.Length;
+
+            if (passwordLength < 8 || passwordLength > 30)
+            {
+                return false;
+            }
+
+            string symbols = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
+            bool validateNums = password.Any(c => Char.IsDigit(c));
+            bool validateCaps = password.Any(c => (int)c >= 65 && (int)c <= 90);
+            bool validateSymbols = password.Intersect(symbols).Any();
+            bool validateNoPasswordKeyword = !(password.ToLower().Contains("password"));
+
+            return validateNums && validateCaps && validateSymbols && validateNoPasswordKeyword;
+        }
+
+
+        /// <summary> Returns a boolean of whether the given password passes the following validation criteria using Regular Expressions (Regex)
+        /// <para>Validation Criteria:</para>
+        /// <para>1. Must be between 8-30 characters in length</para>
+        /// <para>2. Must contain at least 1 number</para>
+        /// <para>3. Must contain at least 1 capital letter (A-Z)</para>
+        /// <para>4. Must contain at least 1 symbol (includes spaces)</para>
+        /// <para>5. Must NOT contain the word "password", regardless of case</para>
+        /// </summary>
+        public bool ValidatePasswordREGEX(string password)
+        {
+            int passwordLength = password.Length;
+
+            if (passwordLength < 8 || passwordLength > 30)
+            {
+                return false;
+            }
+
+            Regex patternNums = new Regex("[0-9]+");
+            Regex patternCaps = new Regex("[A-Z]+");
+            Regex patternNoSymbols = new Regex("^[a-zA-Z0-9]*$");
+            Regex patternPasswordKeyword = new Regex("password");
+
+            bool validateNums = patternNums.IsMatch(password);
+            bool validateCaps = patternCaps.IsMatch(password);
+            bool validateSymbols = !(patternNoSymbols.IsMatch(password));
+            bool validateNoPasswordKeyword = !(patternPasswordKeyword.IsMatch(password.ToLower()));
+
+            return validateNums && validateCaps && validateSymbols && validateNoPasswordKeyword;
+        }
+    }
+}
---
+++ EXERCISES/PasswordCheckerNET6/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+namespace PasswordCheckerNET6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string badPassword__password_keyword = "iAmAvERy!sECure123PAssWord!7";
+            string badPassword__no_symbols = "iAmAvERysECure1237";
+            string badPassword__no_capitals = "iamvery!secure123!7";
+            string badPassword__morethan31chars = "iAmAvERysECure123PAssWord7";
+            string badPassword__lessthan8chars = "vEry1!";
+            string badPassword_no_nums = "iAmAvERy!sECurePAssWord!";
+
+            string goodPassword = "iAmAvERy!sECure123!7";
+
+
+            PasswordCheckerValidation passwordCheckerValidation = new PasswordCheckerValidation();
+
+
+            Console.WriteLine(passwordCheckerValidation.ValidatePasswordLINQ(badPassword__password_keyword));
+            Console.WriteLine(passwordCheckerValidation.ValidatePasswordLINQ(badPassword__no_symbols));
+            Console.WriteLine(passwordCheckerValidation.ValidatePasswordLINQ(badPassword__no_capitals));
+            Console.WriteLine(passwordCheckerValidation.ValidatePasswordLINQ(badPassword__morethan31chars));
+            Console.WriteLine(passwordCheckerValidation.ValidatePasswordLINQ(badPassword__lessthan8chars));
+            Console.WriteLine(passwordCheckerValidation.ValidatePasswordLINQ(badPassword_no_nums));
+            Console.WriteLine(passwordCheckerValidation.ValidatePasswordLINQ(goodPassword));
+
+            
+            Console.WriteLine("\n\n");
+
+
+            Console.WriteLine(passwordCheckerValidation.ValidatePasswordREGEX(badPassword__password_keyword));
+            Console.WriteLine(passwordCheckerValidation.ValidatePasswordREGEX(badPassword__no_symbols));
+            Console.WriteLine(passwordCheckerValidation.ValidatePasswordREGEX(badPassword__no_capitals));
+            Console.WriteLine(passwordCheckerValidation.ValidatePasswordREGEX(badPassword__morethan31chars));
+            Console.WriteLine(passwordCheckerValidation.ValidatePasswordREGEX(badPassword__lessthan8chars));
+            Console.WriteLine(passwordCheckerValidation.ValidatePasswordREGEX(badPassword_no_nums));
+            Console.WriteLine(passwordCheckerValidation.ValidatePasswordREGEX(goodPassword));
+        }
+    }
+}
